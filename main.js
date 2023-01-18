6579f467eae897838730ed1c75ada250438d9f7b
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-01-18 11:19:22 +0000

    1

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const toTop = document.querySelector("#bottomtoUp");
+
+
+const maintop = document.querySelector(".bottomtoUp");
 window.addEventListener("scroll",() => {
     if(window.pageYOffset > 100){
-        toTop.classList.add("active");
+        maintop.classList.add("active");
     }
     else{
-        toTop.classList.remove("active");
+        maintop.classList.remove("active");
     }
 })
