1d190f02097dc29b99286fd30db9cd8bdac59447
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-01-18 12:21:57 +0000

    added backtotop

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 
 
-const maintop = document.querySelector(".bottomtoUp");
 window.addEventListener("scroll", () => {
+    let maintop = document.querySelector(".bottomtoUp");
     if(window.pageYOffset > 100){
-        maintop.classList.add("active");
+        maintop.classList.add("active")
     }
     else{
         maintop.classList.remove("active");
     }
 })
+
+
+
+function top(){
+   window.pageYOffset =0; 
+}
+
+// top.addEventListener("click", ()=> {
+//     window.pageYOffset = 0;
+// })
+
+console.log(maintop)
