1adc909f585e223f49283d7351bb6b242294f0e3
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-01-19 12:00:46 +0000

    33

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-
-
 window.addEventListener("scroll", () => {
-    let maintop = document.querySelector(".bottomtoUp");
-    if(window.pageYOffset > 100){
-        maintop.classList.add("active")
-    }
-    else{
-        maintop.classList.remove("active");
-    }
-})
-
-
+  let maintop = document.querySelector(".bottomtoUp");
+  if (window.pageYOffset > 100) {
+    maintop.classList.add("active");
+  } else {
+    maintop.classList.remove("active");
+  }
+});
