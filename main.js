056c3eaa85d561c99bc3955726ab0b909c9d0b09
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-02-26 10:37:36 +0000

    11

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//for bottom to top scroll button
 window.addEventListener("scroll", () => {
   let maintop = document.querySelector(".bottomtoUp");
   if (window.pageYOffset > 100) {
@@ -10,3 +11,9 @@ window.addEventListener("scroll", () => {
     window.scrollTo(0, 0);
   });
 });
+//Listener for Form
+document.getElementsByClassName('contact-form').addEventListener('submit', submitForm);
+function submitForm(dummy){
+dummy.preventDefault(); 
+console.log(123);
+}
